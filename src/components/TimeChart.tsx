9040389e44015b73b7ef35e64975b4dd2964357c
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2025-08-05 23:06:28 +0530

    Initial commit - Time Tracker App

---
+++ src/components/TimeChart.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Pie } from 'react-chartjs-2'
+import { Chart as chartJS,ArcElement, Tooltip,Legend} from 'chart.js'
+
+chartJS.register(ArcElement,Tooltip,Legend);
+
+interface Props{
+  data: {activity:string,hours:number}[]
+
+}
+
+const COLORS = [
+  '#ff6384', '#36A2b3', '#ffce56', '#340399', '#a788fa',
+  '#FF9F40', '#4BC0C0', '#9966FF', '#C9CBCF', '#FF6384',
+]
+
+const TimeChart = ({data}:Props) => {
+    if (data.length === 0) {
+    return <p className="text-gray-500 text-center">No activity data to display.</p>
+  }
+  const chartData = {
+    labels: data.map((d) => d.activity),
+    datasets: [
+      {
+        label:"hours",
+        data: data.map((d) => d.hours),
+        backgroundColor: data.map((_, i) => COLORS[i % COLORS.length]),
+        borderWidth:1,
+      },
+    ],
+  }
+   const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'right' as const,
+      },
+    },
+  }
+
+  return <Pie data={chartData} options={options}/>
+}
+
+export default TimeChart
---
+++ src/components/TimeForm.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+
+interface Props {
+    onAdd: (activity:string, hours:number) => void
+}
+
+const TimeForm = ({onAdd}:Props) => {
+    const [activity,setActivity] = useState("");
+    const [hours, setHours] = useState("");
+
+    const handleSubmit= () => {
+        if(!activity.trim() || !hours) {
+          alert("Please fill it");
+          return;
+        }
+
+        onAdd(activity , Number(hours));
+
+        setActivity("");
+        setHours("");
+
+    }
+
+
+  return (
+    <div className='space-y-3'>
+        <Input
+        placeholder='Activity (e.g sleep)'
+        value={activity}
+        onChange={(e) => setActivity(e.target.value)}/>
+
+        <Input
+        type='number'
+        placeholder='Hours (e.g 5)'
+        value={hours}
+        onChange={(e) => setHours(e.target.value)}/>
+
+        <Button className='w-full' onClick={handleSubmit}>Add Activity</Button>
+    </div>
+  )
+}
+
+export default TimeForm
